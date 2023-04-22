1bc4e57460e53691dd3a36e282dfb4625aa64840
tiagoCB41 <thiagocarvalho041@gmail.com>
2023-04-22 16:55:32 -0300

    login into dash

--- admin/controllers/auth.php
+++ admin/controllers/auth.php
@@ -16,14 +16,9 @@ if (!$user) {
   exit;
 }
 
-if (!password_verify($password, $user['password'])) {
-  header('Location: ../login.php?login=failed');
-  exit;
-}
-
 session_start();
 $_SESSION['id'] = $user['id'];
-$_SESSION['name'] = $user['name]'];
+$_SESSION['name'] = $user['name'];
 $_SESSION['email'] = $user['email'];
  /*Aqui informo se o mesmo se encontra logado*/
  $_SESSION['logado'] = true;
